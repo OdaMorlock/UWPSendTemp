6cf01717a52407f285a183f1c96d1584b1e767e1
Marcus <Marcus@DATOR>
2020-10-01 12:58:25 +0200

    Add project files.

---
+++ Azurefunction/SendMessageToDevice.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SharedUWPLibrary.Services;
+using SharedUWPLibrary.Models;
+
+
+namespace AzureFunction
+{
+    /*
+    public static class SendMessageToDeviceaa
+    {
+        private static readonly ServiceClient serviceClient = ServiceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("IotHubConnection"));
+
+        [FunctionName("SendMessageToDevice")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            //QueryString Localhost:7071/api/sendmessagetoderice?targetdeviceid=consoleapp&message=dettaarmeddelandet
+            string targetDeviceId = req.Query["targetdeviceid"];
+            string message = req.Query["message"];
+
+            //Http Body = {"targetdericeid": "consoleapp", "message": "Detta är ett meddelande"}
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+
+            var data = JsonConvert.DeserializeObject<BodyMessageModel>(requestBody);
+            targetDeviceId = targetDeviceId ?? data?.TargetDeviceId;
+
+            message = message ?? data?.Message;
+            await DeviceService.SendMessageToDeviceAsync(serviceClient, targetDeviceId, message);
+
+
+
+            return new OkResult();
+        }
+    
+    }*/
+}
---
+++ UWPSendTemp/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Azure.Devices.Client;
+using SharedUWPLibrary.Services;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UWPSendTemp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private static readonly string _conn = "HostName=HubbForOne.azure-devices.net;DeviceId=ConsoleApp;SharedAccessKey=u3bFrWZUgnQq++BMbw/uDQYOQPU4PPEWGBm5f5SEX1w=";
+
+
+
+        private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(_conn, TransportType.Mqtt);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceService.SendMessageAsync(deviceClient).GetAwaiter();
+            DeviceService.ReceiveMessageAsync(deviceClient).GetAwaiter();
+        }
+
+    }
+}
